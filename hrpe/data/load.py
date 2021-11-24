de343fefbe418fee88b644b5783f1b9fde0ce2ac
ari-nz <hello@ari.nz>
2021-11-24 22:49:32 +1300

    Add maxmin load function
    Follow a similar process to minute function

--- hrpe/data/load.py
+++ hrpe/data/load.py
@@ -54,19 +54,39 @@ def load_hh_data(substation, time_start=None, time_end=None):
 def load_maxmin_data(substation, time_start=None, time_end=None):
 
     """
-    Loads the minute data for a given substation
+    Loads the maxmin data for a given substation
     :param substation: the name of the substation
     """
+
+    # whatever
+    check_substation(substation)
+
+    # Get times
+    time_start, time_end = time_check(time_start, time_end)
+
+    # File path - find minute data
     file_dir = os.path.join("data", "raw", substation)
     if not os.path.exists(file_dir):
         raise Exception("The requested substation does not exist")
     files = glob.glob(rf"{file_dir}/*.csv")
 
     for fil in files:
-        if "minute" in fil:
+        if "max_min" in fil:
             filepath = fil
 
-    data = pd.read_csv(filepath, parse_dates=[2, 4, 8])
+    # Read data
+    data = pd.read_csv(filepath, parse_dates=[1])
+
+    # Assert cols
+    expected_cols = ['time', 'value_max', 'value_min']
+    col_names_match = data.columns == expected_cols
+
+    if(not all(col_names_match)):
+        raise Exception(f"Column names do not match\nGot: {data.columns},\n expected: {expected_cols}")
+
+    # Filter by time
+    data = filter_data_by_time(data, time_start, time_end)
+
     return data
 
 
