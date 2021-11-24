10adde0cac33bf7c1272b09f874e7a975166288f
Charles Lan <clan549@aucklanduni.ac.nz>
2021-11-24 23:52:36 +1300

    create calculate_maxmin in deltas.

--- hrpe/data/transform.py
+++ hrpe/data/transform.py
@@ -8,6 +8,7 @@ def minute_data_to_hh_data(data:pd.DataFrame):
 
     df = data.copy()
 
+    # Check if this has been run.
     if not "period_time" in  df.columns:
         df = make_datetime_features(df)
 
@@ -15,11 +16,14 @@ def minute_data_to_hh_data(data:pd.DataFrame):
     # hh_data = df.groupby("period_time").agg({"value": ["max", "min", "mean"],"period": ['first'],"day_of_week":['first'],"month_of_year":['first'],"year":['first']})
     hh_data = df.groupby("period_time").agg({"value": ["max", "min", "mean"]})
     hh_data.columns = ['_'.join(col) for col in hh_data.columns.to_flat_index()]
+
     # Dealing with non-value columns.
     hh_period_data = df.groupby("period_time").first()[['period','day_of_week','month_of_year','year']]
 
+    #join period data back to main hhdataframe.
     hh_data = hh_data.join(hh_period_data)
 
+    # Reset time index
     hh_data["time"] = hh_data.index
 
     hh_data.reset_index(drop=True,inplace = True)
--- hrpe/features/deltas.py
+++ hrpe/features/deltas.py
@@ -10,21 +10,32 @@ def calculate_deltas(data):
 	delta min - mean value over the 30 minute perod subtracted by the minimum value.
 	range -  max subtracted by min
     """
+
     # Check if time period has been added.
     # If not, run datetime features.
 
     hh_data = data.copy()
 
-    # Aggregrate to max min and mean
+    # Calcs delta.
     hh_data["delta_max"] = hh_data["value_max"] - hh_data["value_mean"]
     hh_data["delta_min"] = hh_data["value_mean"] - hh_data["value_min"]
     hh_data['range'] = hh_data["value_max"] - hh_data["value_min"]
 
     return hh_data
 
+
 def calculate_maxmin(data):
     """
-    take  Half hourly data, work out value min and value max
+    take  Half hourly data, Given deltas, work out values.
     Add as columns
     return data
     """
+
+    hh_data = data.copy()
+
+    # Aggregrate to max min and mean
+    hh_data["value_max"] = hh_data["delta_max"] + hh_data["value_mean"]
+    hh_data["value_min"] = hh_data["value_mean"] - hh_data["delta_min"]
+
+    return hh_data
+
