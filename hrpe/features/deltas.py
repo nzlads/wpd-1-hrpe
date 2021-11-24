f4b4b3b6ebb6f65104d85c0051743612cec6fad5
Charles Lan <clan549@aucklanduni.ac.nz>
2021-11-24 23:34:28 +1300

    Added transforms and deltas functions

--- hrpe/features/deltas.py
+++ hrpe/features/deltas.py
@@ -8,17 +8,17 @@ def calculate_deltas(data):
     return hh_data with extra columns:
 	delta max - maximum value over the 30 minute perod subtracted by the mean.
 	delta min - mean value over the 30 minute perod subtracted by the minimum value.
-	range - delta max - delta min
+	range -  max subtracted by min
     """
     # Check if time period has been added.
     # If not, run datetime features.
 
-    df = data.copy()
+    hh_data = data.copy()
 
     # Aggregrate to max min and mean
     hh_data["delta_max"] = hh_data["value_max"] - hh_data["value_mean"]
     hh_data["delta_min"] = hh_data["value_mean"] - hh_data["value_min"]
-    hh_data['range'] = hh_data["delta_max"] - hh_data["delta_min"]
+    hh_data['range'] = hh_data["value_max"] - hh_data["value_min"]
 
     return hh_data
 
